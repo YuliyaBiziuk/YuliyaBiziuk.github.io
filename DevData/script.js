21bb1a5b0de444ad12c87c5e1407dcaaa8936a25
Yuliya <yuliyab.design@gmail.com>
2019-05-16 18:34:26 -0400

    Adding homeworkfile

---
+++ DevData/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("button").click(function(){
+    $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php"  , function(result){
+            $.each(result, function(key, value){
+            $("#dane-programisty").append("<b>" + key + ":" + "</b>" + " " + value + "<br>");
+            });
+        });
+    });
+});
