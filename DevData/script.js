3965c2df08dc1234da9a066bc2f588379b0a7681
Yuliya <yuliyab.design@gmail.com>
2019-05-17 20:21:07 -0400

    Add fixed file

--- DevData/script.js
+++ DevData/script.js
@@ -1,3 +1,5 @@
+$('<div></div>').attr('id','dane-programisty').appendTo('body');
+
 $(document).ready(function(){
     $("button").click(function(){
     $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php"  , function(result){
